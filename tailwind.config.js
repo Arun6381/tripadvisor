77487b4d31b089294a791dd6dc59017348d3148f
Arun6381 <kumararun28092@gmail.com>
2024-05-27 14:38:25 +0530

    updated with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        "hero-pattern": "url('/img/hero-pattern.svg')",
+      }),
+    },
+  },
+  plugins: [],
+};
